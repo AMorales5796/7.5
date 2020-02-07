827179cb7530dfcd55931bd61e7e3c2143086218
AMorales5796 <54290204+AMorales5796@users.noreply.github.com>
2020-02-07 13:17:36 -0800

    Updated

--- src/MyPetsDriver.java
+++ src/MyPetsDriver.java
@@ -2,21 +2,21 @@ public class MyPetsDriver
 {
     public static void main (String args[])
     {
-        Dog Rex = new Dog("Rex", "German Shepherd", 2, 160.2, "Three times a day");
-        //System.out.println(Rex);
+        Dog Rex = new Dog("Rex", "German Shepherd", 2, 160.2, "Kibbles");
+        System.out.println(Rex);
 
-        Lizard Zuckerberg = new Lizard("Zuckerberg","Blue-Tounged-Skink", 8, 2, "Twice a day");
-        //System.out.println(Zuckerberg);
+        Lizard Zuckerberg = new Lizard("Zuckerberg","Blue-Tounged-Skink", 8, 2, "Pineapple");
+        System.out.println(Zuckerberg);
 
-        Bird Speedy = new Bird("Speedy",33,20.4,"Blue",40.5,"Roadrunner","Four times a day");
-        //System.out.println(Speedy);
+        Bird Speedy = new Bird("Speedy",33,20.4,"Blue",40.5,"Roadrunner","Seeds");
+        System.out.println(Speedy);
 
-        System.out.println("Rex says: " + Rex.Speak());
-        System.out.println("Zuckerberg says: " + Zuckerberg.Speak());
-        System.out.println("Speedy says: " + Speedy.Speak());
+        //System.out.println("Rex says: " + Rex.Speak());
+        //System.out.println("Zuckerberg says: " + Zuckerberg.Speak());
+        //System.out.println("Speedy says: " + Speedy.Speak());
 
         //Check unit conversion
-        System.out.println("Rex's weight in pounds is: " + Rex.AmericanFreedomUnits());
+        //System.out.println("Rex's weight in pounds is: " + Rex.AmericanFreedomUnits());
 
         //Pet secret = new Pet("Secret",7, 40.2); Pet is an abstract class that cannot be instantiated.
     }
